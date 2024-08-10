fb6e06c3627d4727e8ec45026c96710a5d64af62
Bilal0335 <bilalhussain42201.com>
2024-08-10 11:58:30 +0500

    update

--- Project/genimini_chatbot.py
+++ Project/genimini_chatbot.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import google.generativeai as genai
 API_KEY = "AIzaSyBbHkLAjGRRhY73L99nQ9siY__8oPgVzRs"
 genai.configure(api_key=API_KEY)
@@ -9,6 +10,15 @@ def getResponseFromModel(user_input):
     response = model.generate_content(user_input)
     return response.text
 
-user_input = input("Enter Your Prompt = ")
-output = getResponseFromModel(user_input)
-print(output)
+
+st.title("Gemini Chatbot")
+user_input = st.text_input("Enter Your Prompt")
+if st.button("Get Response"):
+    if user_input:
+        output = getResponseFromModel(user_input)
+        st.write(f"ChatBot Response: {output}")
+    else:
+        st.write("PLease enter a prompt.")
+        
+
+# print(output)
